2062c59db6620e9498aae1da89445105bf54630a
Jay Ganatra <ganatrajay2000@gmail.com>
2024-11-23 21:22:17 -0500

    actually linking db to ui

--- README.md
+++ README.md
@@ -5,8 +5,8 @@
 - [x] Make it deploy (vercel)
 - [x] Scaffold basic ui with mock data
 - [x] Tidy up build process
-- [ ] Actually set up a database (neon)
-- [ ] Attach database to UI
+- [x] Actually set up a database (neon)
+- [x] Attach database to UI
 - [ ] Add Auth (w/ clerk)
 - [ ] Add image upload
 - [ ] Error Management (w/ Sentry)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,40 +3,25 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/GwkyXx3MczUvnQRNDDlvpWEzbdJNVZAuP2ma1T7LCstwRjgK",
-  "https://utfs.io/f/GwkyXx3MczUvORfx9FS95yO1erXlpMKmaRcStsTbIjinCBfx",
-  "https://utfs.io/f/GwkyXx3MczUvhMqI32xSnbwMciChDqo930BTpfWasrlQHF1j",
-  "https://utfs.io/f/GwkyXx3MczUvkAoX8v4KH9yImnNDUhFY8jC16sZLB7wGbgQa",
-  "https://utfs.io/f/GwkyXx3MczUvVNNuBzofNzFVUuK5LTsfXoJHYrBa0PqkbAD6",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
-  console.log(posts);
+  console.log(images);
   return (
     <main className="px-2">
       <div className="flex flex-wrap gap-2">
-        {posts.map((post) => (
-          <div key={post.id} className="">
-            {post.name}
-          </div>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages].map((image, idx) => (
-          <div key={image.id + "-" + idx} className="w-48">
+        {[...images, ...images, ...images].map((image, idx) => (
+          <div key={image.id + "-" + idx} className="flex w-48 flex-col">
             <Image
               width={2048}
               height={1152}
               src={image.url}
               alt="Gallery Image"
-              className="h-auto w-full rounded-lg object-cover"
+              className="h-auto w-full rounded object-cover"
             />
+            <div className="">{image.name}</div>
           </div>
         ))}
       </div>
